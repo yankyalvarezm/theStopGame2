5beac072a3f5c771a7c5976bac471038ea2c4b82
yanky alvarez <yankyalvarez@yankys-iMac.attlocal.net>
2023-09-14 18:51:08 -0400

    path changes

--- js/play.js
+++ js/play.js
@@ -1,5 +1,5 @@
 document.getElementById('stop').addEventListener('click', function () {
-    window.location.href = '../index.html'
+    window.location.href = './index.html'
 })
 
 console.log('play.js connected')
